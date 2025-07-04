e128a5669b2d8860f9887c6f094042ad95484ea5
zsbeih <zsbeih@umich.edu>
2025-07-04 15:42:45 -0400

    update ngrok with static url for backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,5 @@
-// Update this URL whenever you restart ngrok to test the python backend
-
 const config = {
-  API_BASE_URL: 'https://631d-24-35-46-77.ngrok-free.app'
+  API_BASE_URL: 'https://anchovy-musical-louse.ngrok-free.app'
 };
 
 export default config; 
