16c835f21c0c28e0c894bd374c8af3957d17c7d7
zsbeih <zsbeih@umich.edu>
2025-07-04 01:43:30 -0400

    Environment Variables/Files

---
+++ frontend/src/config.js
@@ -0,0 +1,7 @@
+// Update this URL whenever you restart ngrok to test the python backend
+
+const config = {
+  API_BASE_URL: 'https://631d-24-35-46-77.ngrok-free.app'
+};
+
+export default config; 
